89db5ee17701e01ffe219f6778561da9d3b26b68
Christoph Meier <meier.c@sfeir.lu>
2014-07-23 19:40:25 +0200

    Add foundation Use sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,35 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var stylus = require('gulp-stylus');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var nib = require('nib');
 
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
+gulp.task('connect', function () {
+    connect.server({
+        livereload: true
+    });
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
-  .pipe(connect.reload());
+    gulp.src('*.html')
+        .pipe(connect.reload());
 });
 
-gulp.task('stylus', function() {  
-  gulp.src('assets/stylus/*.styl')
-  .pipe(stylus({use: [nib()], compress: true}))
-  .pipe(rename({ extname: '.min.css' }))
-  .pipe(gulp.dest('www/css'))
-  .pipe(connect.reload());
+gulp.task('sass', function () {
+    gulp.src('sass/**')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['*.html'], ['html']);
-  gulp.watch(['assets/stylus/*.styl'], ['stylus']);
+    gulp.watch(['*.html'], ['html']);
+    gulp.watch(['sass/**'], ['sass']);
 });
 
-gulp.task('default', ['connect', 'stylus', 'watch']);
+gulp.task('default', ['connect', 'sass', 'watch']);
